223f3dc5320f541fcc7756c371a07c057d180091
Christian Ulbrich <christian.ulbrich@zalari.de>
2019-12-09 20:06:00 +0100

    refactor components to a + b, add b as a copy of a

--- packages/test-component-a/stencil.config.ts
+++ packages/test-component-a/stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'test-component-1',
+  namespace: 'test-component-a',
   outputTargets: [
     {
       type: 'dist',
