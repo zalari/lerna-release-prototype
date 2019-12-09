4aa78d3c976534283a49c42565330d664e2c40fa
Christian Ulbrich <christian.ulbrich@zalari.de>
2019-12-09 20:14:06 +0100

    add component c depending on a

--- packages/test-component-c/stencil.config.ts
+++ packages/test-component-c/stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'test-component-c',
+  namespace: 'test-component-b',
   outputTargets: [
     {
       type: 'dist',
